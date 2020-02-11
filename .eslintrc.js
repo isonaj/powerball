7b2c6b1c4a6166ae271402fedb53ab55d3a3bea2
Anthony <isonaj@gmail.com>
2020-02-11 11:24:26 +1000

    Added vuetify for layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "plugin:prettier/recommended",
-    "@vue/prettier",
-    "@vue/typescript"
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    '@vue/prettier',
+    '@vue/typescript'
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: '@typescript-eslint/parser'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true
--- src/components/Powerball.vue
+++ src/components/Powerball.vue
@@ -1,67 +1,70 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <div
-        v-for="index in 7"
-        :key="index"
-        :class="{
-          selecting: index - 1 == primaryNumbers.length,
-          selected: index - 1 < primaryNumbers.length
-        }"
-      >
-        {{ primaryNumbers[index - 1] }}
-      </div>
-      <div class="powerball" :class="{ selected: secondaryNumbers[0] }">
-        {{ secondaryNumbers[0] ? secondaryNumbers[0] : 'PB' }}
-      </div>
-      <div class="autoFill" @click="autofill()">
-        <font-awesome-icon icon="bolt" />
-      </div>
-      <div class="clearSelection" @click="clearSelection()">
-        <font-awesome-icon icon="trash-alt" />
-      </div>
-    </div>
-    <div class="picker">
-      <div
-        v-for="index in 35"
-        :key="index + 10"
-        @click="selectPrimary(index)"
-        :class="{ selected: primaryNumbers.indexOf(index) >= 0 }"
-      >
-        <font-awesome-icon
-          v-if="primaryNumbers.indexOf(index) >= 0"
-          icon="times"
-          style="color: #d6cde4"
-        />
-        {{ index }}
-      </div>
-    </div>
-    <div class="selectPowerball">
-      SELECT YOUR POWERBALL
-    </div>
-    <div class="picker">
-      <div
-        v-for="index in 20"
-        :key="index + 50"
-        @click="selectSecondary(index)"
-        :class="{ selected: secondaryNumbers.indexOf(index) >= 0 }"
-      >
-        <font-awesome-icon
-          v-if="secondaryNumbers.indexOf(index) >= 0"
-          icon="times"
-          style="color: #d6cde4"
-        />
-        {{ index }} <i class="fas fa-times"></i>
-      </div>
-    </div>
-    <!--
-    <v-container>
-      <v-row>
-        <v-col> </v-col>
-      </v-row>
-    </v-container>
-    -->
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="2">
+        <strong>1</strong>
+      </v-col>
+      <v-col cols="8">
+        <div class="card">
+          <div
+            class="btn"
+            v-for="index in 7"
+            :key="index"
+            :class="{
+              selecting: index - 1 == primaryNumbers.length,
+              selected: index - 1 < primaryNumbers.length
+            }"
+          >
+            {{ primaryNumbers[index - 1] }}
+          </div>
+          <div class="btn powerball" :class="{ selected: secondaryNumbers[0] }">
+            {{ secondaryNumbers[0] ? secondaryNumbers[0] : 'PB' }}
+          </div>
+        </div>
+        <div class="picker">
+          <div
+            v-for="index in 35"
+            :key="index + 10"
+            @click="selectPrimary(index)"
+            :class="{ selected: primaryNumbers.indexOf(index) >= 0 }"
+          >
+            <font-awesome-icon
+              v-if="primaryNumbers.indexOf(index) >= 0"
+              icon="times"
+              style="color: #d6cde4"
+            />
+            {{ index }}
+          </div>
+        </div>
+        <div class="selectPowerball">
+          SELECT YOUR POWERBALL
+        </div>
+        <div class="picker">
+          <div
+            v-for="index in 20"
+            :key="index + 50"
+            @click="selectSecondary(index)"
+            :class="{ selected: secondaryNumbers.indexOf(index) >= 0 }"
+          >
+            <font-awesome-icon
+              v-if="secondaryNumbers.indexOf(index) >= 0"
+              icon="times"
+              style="color: #d6cde4"
+            />
+            {{ index }} <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols="2" style="display: flex; flex-wrap: wrap;">
+        <div class="btn autoFill" @click="autofill()">
+          <font-awesome-icon icon="bolt" />
+        </div>
+        <div class="btn clearSelection" @click="clearSelection()">
+          <font-awesome-icon icon="trash-alt" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -90,28 +93,26 @@ export default Vue.extend({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container {
-  max-width: 400px;
-}
 .card {
-  display: flex;
   max-width: 400px;
   margin-bottom: 15px;
   display: flex;
   flex-wrap: wrap;
 }
-.card div {
-  align-items: center;
+.btn {
   display: inline-block;
   box-sizing: border-box;
-  color: white;
-  background-color: white;
-  border: 1px solid #d3d3d3;
   border-radius: 50%;
   width: 40px;
   height: 40px;
   margin-right: 4px;
+  border: 1px solid #d3d3d3;
   font-weight: bolder;
+  color: white;
+}
+.card div {
+  align-items: center;
+  background-color: white;
   /*box-shadow: 5px 5px 5px grey;*/
   /*filter: drop-shadow(3px 3px 4px #444444);*/
 }
@@ -150,15 +151,16 @@ export default Vue.extend({
 .picker div.selected {
   background-color: #e9e4f1;
 }
-.card div.autoFill {
+.autoFill {
   background-color: #6c4498;
 }
-.card div.clearSelection {
+.clearSelection {
   background-color: #767676;
 }
 .selectPowerball {
   color: white;
   background-color: #889bab;
   font-size: 0.8em;
+  max-width: 400px;
 }
 </style>
